518f3af934b8622b7b77e3453607bcba7964f8a5
nguyenhongan15 <anngh2004@gmail.com>
2025-12-16 21:41:43 +0700

    Fix register

--- frontend/src/config/env.js
+++ frontend/src/config/env.js
@@ -1,2 +1,2 @@
-export const API_BASE =
-  import.meta.env.VITE_API_BASE_URL.replace("/api", "");
+export const API_BASE = import.meta.env.VITE_API_BASE_URL;
+export const API_ORIGIN = import.meta.env.VITE_API_BASE_URL.replace("/api", "");
